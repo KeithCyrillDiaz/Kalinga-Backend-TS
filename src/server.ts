f3519814fa38ee89d5a977eecd32ffa35db7bd14
KeithCyrillDiaz <keithcyrill.diaz@tup.edu.ph>
2024-08-01 12:00:10 +0800

    first cimmit

---
+++ src/server.ts
@@ -0,0 +1,23 @@
+console.log("Hello TS")
+
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import compression from 'compression';
+// import router from './router';
+import dotenv from 'dotenv';
+
+
+const app = express();
+
+app.use(cors({
+    credentials: true
+}))
+
+app.use(compression())
+app.use(cookieParser())
+app.use(bodyParser.json())
+
+const server = http.createServer(app)
